3c758edbc3810bf2955e9863e5164a9ffb0a016d
Bobi Bayu Afrianto <bobyaf47@gmail.com>
2025-06-06 05:38:47 +0700

    Add files via upload

---
+++ swiper-init.js
@@ -0,0 +1,20 @@
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3, // 
+  spaceBetween: 30,
+  loop: false,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1
+    },
+    768: {
+      slidesPerView: 2
+    },
+    1024: {
+      slidesPerView: 3
+    }
+  }
+});
